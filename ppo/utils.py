9ef151c9f08b8bc4f961ebdd88b96d9c31e3ddf9
AsadJeewa <asad.jeewa@gmail.com>
2026-08-03 17:07:23 +0200

    feat: MORL eval

--- ppo/utils.py
+++ ppo/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from morl_baselines.common.performance_indicators import hypervolume, sparsity, expected_utility
 
 class RunningMeanStd:
     """
@@ -34,3 +35,65 @@ class RunningMeanStd:
         # Clip the standard deviation to prevent division by zero
         std = np.sqrt(self.var).clip(min=1e-8)
         return np.clip((x - self.mean) / std, -10, 10)
+
+def evaluate_agent_metrics(pareto_archive,ref_point, n_to_select=None):
+    """
+    Compute MORL performance metrics from Pareto archive.
+    """
+
+    if len(pareto_archive.evaluations) == 0:
+        return {}
+
+    # Get stored rewards and weights
+    rewards = np.asarray(pareto_archive.evaluations)
+
+    weights = np.asarray([
+    ind[0] for ind in pareto_archive.individuals
+    ])
+
+    # Pareto filtering
+    mask = np.ones(len(rewards), dtype=bool)
+
+    for i in range(len(rewards)):
+        for j in range(len(rewards)):
+            if (
+                np.all(rewards[j] >= rewards[i])
+                and np.any(rewards[j] > rewards[i])
+            ):
+                mask[i] = False
+                break
+
+    front = rewards[mask]
+
+    if len(front) == 0:
+        return {}
+
+    # Optional downsampling for expensive HV
+    if n_to_select is not None and len(front) > n_to_select:
+        indices = np.random.choice(
+            len(front),
+            n_to_select,
+            replace=False
+        )
+        hv_points = front[indices]
+    else:
+        hv_points = front
+
+
+    metrics = {
+        "eval/hypervolume": hypervolume(
+            ref_point=ref_point,
+            points=hv_points
+        ),
+
+        "eval/sparsity": sparsity(front),
+
+        "eval/eum": expected_utility(
+            front,
+            weights[mask]
+        ),
+
+        "eval/cardinality": len(front),
+    }
+
+    return metrics
