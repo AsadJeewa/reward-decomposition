8879594aa34dc16c22ccb14b888108848895448b
AsadJeewa <asad.jeewa@gmail.com>
2026-08-26 12:57:03 +0200

    featL sweeps

---
+++ sweep.py
@@ -0,0 +1,24 @@
+import wandb
+
+from main_ppo import run_ppo
+
+
+def train():
+    with wandb.init():
+        config = wandb.config
+
+        run_ppo(
+            env_id=config.env_id,
+            env_is_discrete=config.env_is_discrete,
+            total_timesteps=config.total_timesteps,
+            learning_rate=config.learning_rate,
+            gamma=config.gamma,
+            entropy_loss_coefficient=config.entropy_loss_coefficient,
+            diversity_scale=config.diversity_scale,
+            seed=config.seed,
+            use_wandb=True,
+        )
+
+
+if __name__ == "__main__":
+    train()
