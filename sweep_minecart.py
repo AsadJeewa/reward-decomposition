3ca7f0c362c818d145235042b95043790bb2e618
AsadJeewa <asad.jeewa@gmail.com>
2026-09-23 09:18:42 +0200

    feat: minecart updated sweep

--- sweep_minecart.py
+++ sweep_minecart.py
@@ -7,22 +7,14 @@ def train():
     with wandb.init():
         config = wandb.config
 
-        # config_name = (
-        #     f"lr={config.learning_rate}_"
-        #     f"div={config.diversity_scale}_"
-        #     f"ent={config.entropy_loss_coefficient}"
-        # )
-
-        # wandb.run.group = config_name
-
         run_ppo(
             env_id=config.env_id,
             env_is_discrete=config.env_is_discrete,
             total_timesteps=config.total_timesteps,
             learning_rate=config.learning_rate,
+            num_rollout_steps=config.num_rollout_steps,
+            update_epochs=config.update_epochs,
             gamma=config.gamma,
-            entropy_loss_coefficient=config.entropy_loss_coefficient,
-            diversity_scale=config.diversity_scale,
             seed=config.seed,
             use_wandb=True,
         )
